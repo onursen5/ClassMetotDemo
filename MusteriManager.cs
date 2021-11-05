85b3a975427f65dee3661488ed0c437f5c957c69
onursen <81871714+onursen5@users.noreply.github.com>
2021-11-06 00:01:07 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Kaydınız oluşturuldu"+musteri.MusteriAd+musteri.MusteriSoyad);
+        }
+        public void Sil(Musteri musteri2)
+        {
+            Console.WriteLine("Kaydınız Silindi"+musteri2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            int musteriId = 1;
+            string musteriAd = "Ahmet";
+            string musterisoyad = "IŞIK";
+
+            Musteri musteri2 = new Musteri();
+            string musteriAdi = "Mehmet";
+        }
+    }
+}
